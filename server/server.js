093dd84cc7e6c0598d57da299688dcbe43293c89
Goran SD <g.cvetic23@gamail.com>
2021-10-18 17:02:25 -0700

    heroku

--- server/server.js
+++ server/server.js
@@ -12,9 +12,9 @@ app.use("/search", searchRouter);
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname, "client/build"));
+  app.use(express.static(__dirname, "build"));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'build/index.html'));
   });
 }
 
